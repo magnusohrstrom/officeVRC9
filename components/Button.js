0eda0e01817e68017b452377db16f8e6a62995f5
magnusohrstrom <magnusohrstrom330@gmail.com>
2018-02-15 15:25:04 +0100

    Add: Proptypes and Button component

---
+++ components/Button.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    AppRegistry,
+    asset,
+    Pano,
+    Scene,
+    Text,
+    View,
+    Image, 
+    Box,
+    VrButton
+  } from 'react-vr';
+
+const Button = (props) => {
+    return (
+        <VrButton style={props.style}
+                    onClick={props.onClick}>
+            <Image style={{width:2, height:2}}
+                source={asset(props.sourceUrl)}
+                    inset={[0.2,0.2,0.2,0.2]}
+                        insetSize={[0.05,0.45,0.55,0.15]} >
+            </Image>
+        </VrButton>
+    );
+}
+
+export default Button;
+
+
+
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    style: PropTypes.object.isRequired,
+    sourceUrl:PropTypes.string.isRequired,
+    className: PropTypes.string
+  }
+  
--- index.vr.js
+++ index.vr.js
@@ -3,30 +3,68 @@ import {
   AppRegistry,
   asset,
   Pano,
+  Scene,
   Text,
   View,
+  Image, 
+  Box,
+  VrButton
 } from 'react-vr';
+import Button from './components/Button';
 
 export default class officeVRC9 extends React.Component {
+  state = {
+    room:1
+  }
+
+  log = () => {
+    console.log('hejsy');
+  }
+
+
   render() {
     return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
+   
+     
+     <View style={{display:'flex', flexDirection:'column', height:"200px", width:"200px"}}>
+      <Pano style={{marginBottom:60}} onLoad = {()=>{console.log('loadyd');
+      }}source={asset('skola.jpg')}/>
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            width: 2,
+            alignItems: 'stretch',
+            transform: [{translate: [-1, 1, -5]}],
+          }}>
+
+          <View pointerEvents = 'auto' onPress = {this.log} style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
+            <Text pointerEvents = 'auto'   style={{fontSize: 0.2, textAlign: 'center'}}>Red</Text>
+          </View>
+
+        </View>
+        <Button style= {{width: 2.7, 
+                transform:[{translate: [-10, 0, -10]},
+                {rotateY:"90deg"}]}}
+                onClick = {this.log}
+                sourceUrl = 'magnus2.jpg'
+                /> 
+        <Text onPress = {this.log}
           style={{
             backgroundColor: '#777879',
+
             fontSize: 0.8,
             fontWeight: '400',
             layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
-            textAlign: 'center',
+            textAlign: 'left',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [{translate: [0, 0, -7]}],
           }}>
           hello
         </Text>
-      </View>
+      </View> 
+
     );
   }
 };
